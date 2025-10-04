ea4f6d9c40037007f0afb7c88ee562ae482a543a
AshadJutt <ashadgamers1234@gmail.com>
2025-10-04 21:16:36 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,4 +21,5 @@ var countDownDate = new Date("December 25, 2022 00:00:00").getTime();
 var x=setInterval(function(){var e=countDownDate-new Date().getTime();$("#sale-d").html(Math.floor(e/864e5)),$("#sale-h").html(Math.floor(e%864e5/36e5)),$("#sale-m").html(Math.floor(e%36e5/6e4)),$("#sale-s").html(Math.floor(e%6e4/1e3)),e<0&&(clearInterval(x),$(".sale-banner").remove())},1e3);$("#btnCode").click(function(){var e=$("<input>");$("body").append(e),e.val($("#btnCode span").text()).select(),document.execCommand("copy"),e.remove(),Swal.fire("Good job!","You clicked the button!","success"),Swal.fire({title:"Successful",text:"Coupon has been copied to clipboard!",icon:"success",button:"Exit"})});
 
 /* Web Site Policies */
-window.addEventListener("load",function(){window.wpcc.init({"colors":{"popup":{"background":"#1A1C1E","text":"#FFFFFF","border":"#1873D3"},"button":{"background":"#1873D3","text":"#FFFFFF"}},"corners":"small","transparency":"100","content":{"href":"hhtps://yoursite.com/legal/"},"position":"bottom-right"})});
+window.addEventListener("load",function(){window.wpcc.init({"colors":{"popup":{"background":"#1A1C1E","text":"#FFFFFF","border":"#008000"},"button":{"background":"#008000","text":"#FFFFFF"}},"corners":"small","transparency":"100","content":{"href":"hhtps://yoursite.com/legal/"},"position":"bottom-right"})});
+
